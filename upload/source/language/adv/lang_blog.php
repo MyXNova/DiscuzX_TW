e4f68dc27a80126326655cdfe272fcda66d47e16
a950216t <a950216t@gmail.com>
2023-11-26 22:15:04 +0800

    source/language/adv 正體中文化

--- upload/source/language/adv/lang_blog.php
+++ upload/source/language/adv/lang_blog.php
@@ -13,8 +13,8 @@ if(!defined('IN_DISCUZ')) {
 
 $lang = array
 (
-	'blog_name' => '空间 日志广告',
-	'blog_desc' => '展现方式: 日志广告显示于日志右方。',
+	'blog_name' => '空間 網誌廣告',
+	'blog_desc' => '展現方式：網誌廣告顯示於網誌右方。',
 );
 
 ?>
--- upload/source/language/adv/lang_intercat.php
+++ upload/source/language/adv/lang_intercat.php
@@ -13,14 +13,14 @@ if(!defined('IN_DISCUZ')) {
 
 $lang = array
 (
-	'intercat_name' => '论坛 分类间广告',
-	'intercat_desc' => '展现方式: 论坛分类间广告显示于版块列表相邻的两个版块分类之间，可使用 960x60 或其他尺寸图片和 Flash 的形式。当前页面有多个分类间广告时，系统会从中抽取与版块分类数相等的条目进行随机显示。<br />价值分析:由于出现在版块列表页比较明显的位置，广告展示效果较好，但是过多过大的广告可能会招致访问者反感。 ',
-	'intercat_fids' => '投放分区',
-	'intercat_fids_comment' => '设置广告投放的版块分区',
+	'intercat_name' => '論壇 分類間廣告',
+	'intercat_desc' => '展現方式：論壇分類間廣告顯示於版區列表相鄰的兩個版區分類之間，可使用 960x60 或其他大小圖片和 Flash 的形式。目前頁面有多個分類間廣告時，系統會從中抽取與版區分類數相等的數量進行隨機顯示。<br />價值分析：由於出現在版區列表頁比較明顯的位置，廣告展示效果較好，但是過多過大的廣告可能會招致瀏覽者反感。 ',
+	'intercat_fids' => '投放分區',
+	'intercat_fids_comment' => '設定廣告投放的版區分區',
 	'intercat_position' => '投放位置',
-	'intercat_position_comment' => '设置广告在哪个分区的下方显示',
-	'intercat_position_random' => '所有位置随机',
-	'intercat_position_fav' => '收藏栏',
+	'intercat_position_comment' => '設定廣告在哪個分區的下方顯示',
+	'intercat_position_random' => '所有位置隨機',
+	'intercat_position_fav' => '收藏欄',
 );
 
 ?>
